69257dd4edeca71b6b29ff390e7f5603785f6d64
raccoon1990 <94162136+raccoon1990@users.noreply.github.com>
2021-11-15 15:57:31 +0900

    Update Start.java

--- HelloWorld/src/Start.java
+++ HelloWorld/src/Start.java
@@ -3,7 +3,9 @@ public class Start {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("slave branch");	
+		System.out.println("slave branch");
+		
+		System.out.println("github 수정");
 	}
 
 }
